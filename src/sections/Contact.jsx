2d3a2a9bf515420c4992c5997d25dd145d3794eb
Muhammad Faizan <130775084+MuhammadFaizan-prog@users.noreply.github.com>
2025-06-21 00:32:58 +0500

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+'service_26mx2yr',
+        'template_lw3k1cd',
         {
           from_name: form.name,
           to_name: 'Faizan',
@@ -31,7 +31,7 @@ const Contact = () => {
           to_email: 'muhammadfaizanali18@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        'gZeR4UlmgkNSkvDLC',
       )
       .then(
         () => {
